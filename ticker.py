c3011159c24c7e639e1ffe8c0831600b415a1995
liw0 <wol.linu@gmail.com>
2013-09-13 10:47:56 +0200

    Create ticker.py

---
+++ ticker.py
@@ -0,0 +1,14 @@
+import requests, json
+from datetime import datetime
+
+r = requests.get("https://www.bitstamp.net/api/ticker")
+t = json.loads(r.content)
+high = t["high"]
+last = t["last"]
+low  = t["low"]
+timestamp = datetime.now().strftime('%b %d %H:%M:%S')
+
+print timestamp
+print "high:", high
+print "low:", low
+print "last:", last
